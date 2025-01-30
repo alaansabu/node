3ee5c31741e0d5204bbc5c3ae4505f08749791a3
alaansabu <alansabu6046@gmail.com>
2025-01-31 02:40:23 +0530

    third commit

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
-console.log("hi");
-setTimeout(function(){
+const { readFile, writeFile } = require('fs');
+const util = require('util');
 
-    console.log("hey there55");
-    
-},0)
+// Promisify readFile and writeFile
+const readFilePromise = util.promisify(readFile);
+const writeFilePromise = util.promisify(writeFile);
 
-console.log("hi all");
+const start = async () => {
+    try {
+        // Read the files asynchronously
+        const first = await readFilePromise("./content/mod1.txt", "utf-8");
+        const second = await readFilePromise("./content/mod.txt", "utf-8");
 
+        // Write to a new file asynchronously
+        await writeFilePromise("./content/subfolder/minde-gernade.txt", `${first} ${second}`);
+
+        console.log("File written successfully!");
+        console.log(first, second);
+    } catch (error) {
+        console.error("Error:", error.message);
+    }
+};
+
+// Start the process
+start();
---
+++ event-module.js
@@ -0,0 +1,9 @@
+const eventEmitter = require('events');
+const customEmitter =new  eventEmitter();
+
+customEmitter.on('response',() => {
+    console.log("data recived");
+    
+})
+
+customEmitter.emit('response')
